6285c03d45920b25d5296651f88d875e7190b73f
Sergey <xxsmotur@gmail.com>
2020-04-17 17:41:55 +0500

    Add draft for server implementation

---
+++ server.go
@@ -0,0 +1,83 @@
+// Server container for a Raft Consensus Module. Exposes Raft to the network
+// and enables RPCs between Raft peers.
+//
+// Eli Bendersky [https://eli.thegreenplace.net]
+// This code is in the public domain.
+package raft
+
+import (
+	"log"
+	"net"
+	"net/rpc"
+	"sync"
+)
+
+type Server struct {
+	mu sync.Mutex
+
+	serverId int
+	peerIds  []int
+
+	ready <-chan interface{}
+	quit  chan struct{}
+	wg    sync.WaitGroup
+
+	cm       *ConsensusModule
+	rpcProxy *RPCProxy
+
+	rpcServer *rpc.Server
+	listener  net.Listener
+
+	peers map[int]*rpc.Client
+}
+
+// New server creates a new one server
+func NewServer(id int, peerIds []int, ready <-chan interface{}) *Server {
+	s := new(Server)
+	s.quit = make(chan struct{})
+	return &Server{
+		serverId: id,
+		peerIds:  peerIds,
+		ready:    ready,
+		quit:     make(chan struct{}),
+	}
+}
+
+func (s *Server) Serve() {
+	s.mu.Lock()
+	s.cm = NewConsensusModule(s.serverId, s.peerIds, s, s.ready)
+
+	s.rpcServer = rpc.NewServer()
+	s.rpcProxy = &RPCProxy{cm: s.cm}
+	s.rpcServer.RegisterName("ConsensusModule", s.rpcProxy)
+
+	var err error
+	s.listener, err = net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("[%v] listening at %s", s.serverId, s.listener.Addr())
+	s.mu.Unlock()
+
+	s.wg.Add(1)
+	go func() {
+		defer s.wg.Done()
+
+		for {
+			conn, err := s.listener.Accept()
+			if err != nil {
+				select {
+				case <-s.quit:
+					return
+				default:
+					log.Fatal("accept error:", err)
+				}
+			}
+			s.wg.Add(1)
+			go func() {
+				s.rpcServer.ServeConn(conn)
+				s.wg.Done()
+			}()
+		}
+	}()
+}
